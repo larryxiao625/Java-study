4c127553dd6dad9a25c454b18a6d124efb058554
ray26 <romerxzh@gmail.com>
2018-07-17 23:35:04 +0800

    UPDATE DAY13

--- ObjectProject-myself/src/com/imooc/mechanics/Cat.java
+++ ObjectProject-myself/src/com/imooc/mechanics/Cat.java
@@ -1,5 +1,18 @@
 package com.imooc.mechanics;
 
 public class Cat {
-
+	//属性：昵称，年龄，体重，品种
+	//修改属性的可见性--private(限定只能在当前类内访问)
+	private String name;//昵称
+	private int month;//年龄
+	private double weight;//体重
+	private String species;//品种
+	
+	public Cat() {
+		System.out.println("我是宠物猫~~~~");
+	}
+	
+	public Cat(int month) {
+		
+	}
 }
