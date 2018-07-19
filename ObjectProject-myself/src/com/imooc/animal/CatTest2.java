8d4342d31c7f08d5f7abfc666b7b4bf50d57b1f5
ray26 <romerxzh@gmail.com>
2018-07-19 23:55:10 +0800

    UPDATE DAY14

--- ObjectProject-myself/src/com/imooc/animal/CatTest2.java
+++ ObjectProject-myself/src/com/imooc/animal/CatTest2.java
@@ -1,5 +1,12 @@
 package com.imooc.animal;
+	
 
 public class CatTest2 {
-
+		Cat three=new Cat();
+	public void cattest2(Cat myCat) {
+		System.out.println(myCat.getMonth());
+	}
+	public void main(String[] args) {
+		this.cattest2(three);
+	}
 }
--- SchoolProject/src/com/imooc/model/Student.java
+++ SchoolProject/src/com/imooc/model/Student.java
@@ -6,13 +6,14 @@ public class Student {
 	private String studentNo;
 	private String studentSex;
 	private int studentAge;
-	private Subject studentSubject;//没有赋值之前对象值为null
+	private Subject studentSubject;// 没有赋值之前对象值为null
 
 	// 无参构造方法
 	public Student() {
 
 	}
-	//实现对学号，姓名，性别，年龄的赋值
+
+	// 实现对学号，姓名，性别，年龄的赋值
 	public Student(String studentName, String studentNo, String studentSex, int studentAge) {
 		super();
 		this.setStudentName(studentName);
@@ -21,15 +22,15 @@ public class Student {
 		this.setStudentAge(studentAge);
 
 	}
+
 	// 多参构造方法，实现对全部属性的赋值
-	public Student(String studentName, String studentNo, String studentSex, int studentAge,Subject studentSubject) {
+	public Student(String studentName, String studentNo, String studentSex, int studentAge, Subject studentSubject) {
 		super();
 		this.setStudentName(studentName);
 		this.setStudentNo(studentNo);
 		this.setStudentSex(studentSex);
 		this.setStudentAge(studentAge);
 		this.setStudentSubject(studentSubject);
-
 
 	}
 
@@ -82,13 +83,15 @@ public class Student {
 			this.studentAge = 18;
 		}
 	}
+
 	/**
 	 * 获取专业对象，如果没有实例化，先实例化后返回
+	 * 
 	 * @return
 	 */
 	public Subject getStudentSubject() {
-		if(this.studentSubject==null) {
-			this.studentSubject=new Subject();
+		if (this.studentSubject == null) {
+			this.studentSubject = new Subject();
 		}
 		return studentSubject;
 	}
@@ -99,36 +102,43 @@ public class Student {
 
 	/**
 	 * 学生自我介绍的办法
+	 * 
 	 * @param所报专业名称
 	 * @param学制年限
 	 * @return学生姓名，学号，年龄，性别信息
 	 */
 	public String introduction() {
 		String str = "学生信息如下：\n姓名" + this.getStudentName() + "\n性别" + this.getStudentSex() + "\n学号"
-				+ this.getStudentNo() + "\n年龄" + this.getStudentAge()+"\n所报专业名称"+this.getStudentSubject().getSubjectName()+"\n所报专业年限"+this.getStudentSubject().getSubjectLife();
+				+ this.getStudentNo() + "\n年龄" + this.getStudentAge() + "\n所报专业名称"
+				+ this.getStudentSubject().getSubjectName() + "\n所报专业年限" + this.getStudentSubject().getSubjectLife();
 		return str;
 	}
-	
+
 	/**
 	 * 学生自我介绍的办法
+	 * 
 	 * @param所报专业名称
 	 * @param学制年限
 	 * @return学生姓名，学号，年龄，性别信息
 	 */
-	public String introduction(String subjectName,int subjectLife) {
+	public String introduction(String subjectName, int subjectLife) {
 		String str = "学生信息如下：\n姓名" + this.getStudentName() + "\n性别" + this.getStudentSex() + "\n学号"
-				+ this.getStudentNo() + "\n年龄" + this.getStudentAge()+"\n所报专业名称"+subjectName+"\n所报专业年限"+subjectLife;
+				+ this.getStudentNo() + "\n年龄" + this.getStudentAge() + "\n所报专业名称" + subjectName + "\n所报专业年限"
+				+ subjectLife;
 		return str;
 	}
+
 	/**
 	 * 学生自我介绍的办法
+	 * 
 	 * @param所报专业名称
 	 * @param学制年限
 	 * @return学生姓名，学号，年龄，性别信息
 	 */
 	public String introduction(Subject mySubject) {
 		String str = "学生信息如下：\n姓名" + this.getStudentName() + "\n性别" + this.getStudentSex() + "\n学号"
-				+ this.getStudentNo() + "\n年龄" + this.getStudentAge()+"\n所报专业名称"+mySubject.getSubjectName()+"\n所报专业年限"+mySubject.getSubjectLife();
+				+ this.getStudentNo() + "\n年龄" + this.getStudentAge() + "\n所报专业名称" + mySubject.getSubjectName()
+				+ "\n所报专业年限" + mySubject.getSubjectLife();
 		return str;
 	}
 
