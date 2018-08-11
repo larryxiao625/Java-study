d72c6672a976578d5a1102df0fe2dcf1a496db91
ray26 <romerxzh@gmail.com>
2018-08-11 22:33:33 +0800

    UPDATE DAY26

---
+++ Source/运算符-src/ImoocProj2/src/com/imooc/set/ListDemo1.java
@@ -0,0 +1,19 @@
+package com.imooc.set;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListDemo1 {
+
+	public static void main(String[] args) {
+		List list=new ArrayList();
+		list.add("Java");
+		list.add("C");
+		list.add("C++");
+		list.add("go");
+		list.add("swift");
+		System.out.println(list.size());
+
+	}
+
+}
